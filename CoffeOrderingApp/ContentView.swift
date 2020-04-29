8e241cc7a2102e0620858f8c6e11f9e7a09c6581
genaro_arvizu <lgarvizu@santander.com.mx>
2020-04-29 13:59:34 -0500

    Initial Commit

---
+++ CoffeOrderingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeOrderingApp
+//
+//  Created by Genaro Arvizu on 29/04/20.
+//  Copyright © 2020 GAR. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
